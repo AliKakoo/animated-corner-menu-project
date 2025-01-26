b12c19dfcfedecc2195dce9ffe0616d3ca96c812
AliKakoo <alikakuo@yahoo.com>
2025-01-26 20:00:37 +0330

    add final codes

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ let isMenuActive = false;
 
 toggleBtn.addEventListener("click", () => {
   if (!isMenuActive) {
-    console.log("the menu is active");
+    console.log("منو باز شد");
+
+    menuItem[0].style.transform = "translate(150px, 0)";
+    menuItem[1].style.transform = "translate(150px, 90px)";
+    menuItem[2].style.transform = "translate(90px, 150px)";
+    menuItem[3].style.transform = "translate(0, 150px)";
 
     toggleBtn.classList.add('active');
     
     isMenuActive = true;
 } else {
-    console.log("the menu is not active");
+    console.log("منو بسته شد");
     toggleBtn.classList.remove("active");
+    menuItem.forEach(menuItem => menuItem.style.transform = "translate(0,0)")
     
     isMenuActive = false;
   }
